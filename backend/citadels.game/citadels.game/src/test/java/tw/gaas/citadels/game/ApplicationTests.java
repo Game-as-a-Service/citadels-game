38a90acdcdb458f332db2707433d8261e28da427
timking <timt1026@hotmail.com>
2023-06-14 22:29:39 +0800

    backend project init

---
+++ backend/citadels.game/citadels.game/src/test/java/tw/gaas/citadels/game/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.gaas.citadels.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
