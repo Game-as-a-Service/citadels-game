398c7dbb27e6847fcb348b59f7ac7f410dd9eb63
timking <timt1026@hotmail.com>
2023-06-15 22:09:39 +0800

    backend init test

--- backend/citadels.game/citadels.game/spring/src/main/java/tw/waterballsa/gaas/citadels/spring/CitadelsApplication.java
+++ backend/citadels.game/citadels.game/spring/src/main/java/tw/waterballsa/gaas/citadels/spring/CitadelsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CitadelsApplication {
 
     public static void main(String[] args) {
-
+        //test
         SpringApplication.run(AvalonApplication.class, args);
     }
 }
