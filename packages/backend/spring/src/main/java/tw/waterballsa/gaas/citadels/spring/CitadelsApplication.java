d7ac824ef86cb3db817eb3068bcf494e4c2eab14
LAPTOP-F8HGTF15\ChrisLo <c11037at@gmail.com>
2023-06-18 23:02:10 +0800

    add hello world api

--- packages/backend/spring/src/main/java/tw/waterballsa/gaas/citadels/spring/CitadelsApplication.java
+++ packages/backend/spring/src/main/java/tw/waterballsa/gaas/citadels/spring/CitadelsApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CitadelsApplication {
-
+	// Joe here :)
+	// Hi~
 	public static void main(String[] args) {
 		SpringApplication.run(CitadelsApplication.class, args);
 	}
+
 
 }
