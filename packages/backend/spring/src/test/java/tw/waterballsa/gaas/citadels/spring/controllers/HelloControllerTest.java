798e6a3be8e0c002e9ff69f3baab899715f7d5a6
提姆 <69762490+timt1028@users.noreply.github.com>
2023-07-06 22:45:36 +0800

    test walking skeleton (#73)

---
+++ packages/backend/spring/src/test/java/tw/waterballsa/gaas/citadels/spring/controllers/HelloControllerTest.java
@@ -0,0 +1,23 @@
+package tw.waterballsa.gaas.citadels.spring.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.junit.jupiter.api.Test;
+
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void TestHelloEndPoint() throws Exception {
+         mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("hello world!"));
+    }
+
+}
