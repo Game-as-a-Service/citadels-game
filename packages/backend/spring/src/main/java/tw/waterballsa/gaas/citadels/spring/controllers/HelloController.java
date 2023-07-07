9544b17fc11f81004cae657763c4629fe2ea8ebf
aprilywy <aprilywy@users.noreply.github.com>
2023-07-07 21:06:20 +0800

    CorsConfig setting

--- packages/backend/spring/src/main/java/tw/waterballsa/gaas/citadels/spring/controllers/HelloController.java
+++ packages/backend/spring/src/main/java/tw/waterballsa/gaas/citadels/spring/controllers/HelloController.java
@@ -1,14 +1,22 @@
 package tw.waterballsa.gaas.citadels.spring.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
+    @CrossOrigin(origins = "http://localhost:3001/")
     @GetMapping("/hello")
-    public String getHelloWorld() {
-        return "{\"msg\": \"hello\"}";
+    public ResponseEntity<Map<String, String>> getHelloWorld() {
+        Map<String, String> response = new HashMap<>();
+        response.put("msg", "hello");
+        return ResponseEntity.ok(response);
     }
 
 }
