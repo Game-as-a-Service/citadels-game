2e8e9078a570b803299c8e7d74048503185b1cbd
LAPTOP-F8HGTF15\ChrisLo <c11037at@gmail.com>
2023-07-13 02:06:13 +0800

    使用內嵌式的 mongoDB 進行測試

--- packages/backend/spring/src/test/java/tw/waterballsa/gaas/citadels/spring/CitadelsApplicationTest.java
+++ packages/backend/spring/src/test/java/tw/waterballsa/gaas/citadels/spring/CitadelsApplicationTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import tw.waterballsa.gaas.citadels.spring.repositories.dao.CitadelsGameDAO;
 import tw.waterballsa.gaas.citadels.spring.repositories.data.CitadelsGameData;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public abstract class CitadelsApplicationTest {
     @Autowired
     protected MockMvc mockMvc;
