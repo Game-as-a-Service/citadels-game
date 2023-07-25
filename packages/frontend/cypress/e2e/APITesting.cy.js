512064304c8c4b622ef84f2246e7fb49ffe09c3f
Enya <26k1234o3@gmail.com>
2023-07-25 23:30:20 +0800

    編排修改

--- packages/frontend/cypress/e2e/APITesting.cy.js
+++ packages/frontend/cypress/e2e/APITesting.cy.js
@@ -3,7 +3,10 @@ describe('HTTPRequest', () => {
   // eslint-disable-next-line no-undef
   it('GET Call', () => {
     // eslint-disable-next-line no-unused-expressions, no-undef
-    cy.request('GET', 'https://001f08b9-acb7-4c3a-a54f-a9254b7e8e55.mock.pstmn.io/get?msg=hello')
+    cy.request(
+      'GET',
+      'https://001f08b9-acb7-4c3a-a54f-a9254b7e8e55.mock.pstmn.io/get?msg=hello'
+    )
       .its('status')
       .should('equal', 200)
   })
