345d05b5d7009234a8d7ae2354b0d775c46a1e02
snowman <c11037at@gmail.com>
2023-07-26 23:38:21 +0800

    當登入時儲存名子和前往大廳頁面

--- packages/frontend/js/components/Login.jsx
+++ packages/frontend/js/components/Login.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const [name, setName] = useState('')
+  const navigate = useNavigate()
+
+  const handleNameChange = (event) => {
+    setName(event.target.value)
+  }
+
+  const handleLogin = () => {
+    console.log('userName:', name)
+    localStorage.setItem('userName', name)
+    // TODO: game lobby page not completed
+    navigate('/rooms')
+  }
+
   return (
     <>
       <section className='login'>
         <span className='f-40-w'>WelCome to Citadels Game !</span>
-        <form className='login_borad'>
+        <div className='login_borad' onSubmit={handleLogin}>
           <div className='login_imgwrap'>
             <img src='' />
           </div>
           <div className='f-20-b'>換一個</div>
           <label htmlFor='name' className='f-24-b'>
-            請輸入你的名字：
+            輸入你的名字：
           </label>
           <input
             id='name'
@@ -19,9 +34,13 @@ const Login = () => {
             name='name'
             placeholder='輸入名字'
             className='login_input'
+            value={name}
+            onChange={handleNameChange}
           />
-        </form>
-        <button className='login_button'>進入遊戲</button>
+        </div>
+        <button className='login_button' onClick={handleLogin}>
+          進入遊戲
+        </button>
       </section>
     </>
   )
