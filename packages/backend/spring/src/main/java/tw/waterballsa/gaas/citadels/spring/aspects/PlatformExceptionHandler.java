a5ed044af89427cea28f993bd439190992e63ef2
chiashengchen <peterchen40213@yahoo.com.tw>
2023-08-05 16:04:25 +0800

    Add GlobalHandler for Exception Detail

--- packages/backend/spring/src/main/java/tw/waterballsa/gaas/citadels/spring/aspects/PlatformExceptionHandler.java
+++ packages/backend/spring/src/main/java/tw/waterballsa/gaas/citadels/spring/aspects/PlatformExceptionHandler.java
@@ -1,9 +1,12 @@
 package tw.waterballsa.gaas.citadels.spring.aspects;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import tw.waterballsa.gaas.citadels.exceptions.PlatformException;
 
 @RestControllerAdvice
@@ -13,5 +16,10 @@ public class PlatformExceptionHandler {
     @ExceptionHandler(PlatformException.class)
     public String badRequest(PlatformException exception) {
         return exception.getMessage();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
