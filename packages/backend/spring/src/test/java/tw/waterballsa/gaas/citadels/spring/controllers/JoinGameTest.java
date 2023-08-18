201b812fd3e7a90e034283981eb8f20e2f07a8fc
chiashengchen <peterchen40213@yahoo.com.tw>
2023-08-18 22:24:42 +0800

    Refactor

--- packages/backend/spring/src/test/java/tw/waterballsa/gaas/citadels/spring/controllers/JoinGameTest.java
+++ packages/backend/spring/src/test/java/tw/waterballsa/gaas/citadels/spring/controllers/JoinGameTest.java
@@ -1,67 +1,18 @@
 package tw.waterballsa.gaas.citadels.spring.controllers;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import tw.waterballsa.gaas.citadels.app.repositories.CitadelsGameRepository;
 import tw.waterballsa.gaas.citadels.domain.CitadelsGame;
 import tw.waterballsa.gaas.citadels.domain.Player;
+import tw.waterballsa.gaas.citadels.spring.CitadelsApplicationTest;
 
-import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-//    @Test Saboteur
-//    public void 都好的硬要修() throws Exception {
-//        Player A = defaultPlayer("A");
-//        Player B = defaultPlayer("B");
-//        B.addHandCard(new Repair(ToolName.PICK));
-//
-//        Player C = defaultPlayer("C");
-//
-//        SaboteurGame game = givenGameStarted(A, B, C);
-//
-//        mockMvc.perform(post("/api/games/{gameId}:playCard", game.getId())
-//                        .contentType(APPLICATION_JSON)
-//                        .content("""
-//                                {   "cardType": "REPAIR",
-//                                "playerId": "B",
-//                                  "handIndex": 0,
-//                                  "targetPlayerId": "A"
-//                                }"""))
-//                .andExpect(status().isBadRequest());
-//
-//        var actualGame = findGameById(game.getId());
-//        Player actualA = actualGame.getPlayer("A");
-//
-//        assertTrue(actualA.getTool(ToolName.MINE_CART).isAvailable());
-//        assertTrue(actualA.getTool(ToolName.LANTERN).isAvailable());
-//        assertTrue(actualA.getTool(ToolName.PICK).isAvailable());
-//    }
+public class JoinGameTest extends CitadelsApplicationTest {
 
-@SpringBootTest
-@AutoConfigureMockMvc
-public class JoinGameTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private CitadelsGameRepository citadelsGameRepository;
-
-    private CitadelsGame givenGameStarted(Player ...players) {
-        return citadelsGameRepository.save(new CitadelsGame(asList(players)));
-    }
-
-    private CitadelsGame findById(String gameId) {
-        return citadelsGameRepository.findById(gameId);
-    }
-
-    // 房間的狀態要不要考慮 ?
     @Test
     public void playerJoinsGameSuccessfully() throws Exception {
 
@@ -71,28 +22,26 @@ public class JoinGameTest {
         CitadelsGame game = givenGameStarted(A, B);
 
         // join game
-        // post /games/{gameId}:join
-        // body playerId, playerName
         Player C = new Player("789", "C");
         String jsonBody = "{\"playerName\" : \"" + C.getName() + "\", " +
-                "\"playerId\" : \"" + C.getId() + "\" }";
+                           "\"playerId\" : \"" + C.getId() + "\" }";
         mockMvc.perform(post("/games/{gameId}:join", game.getId())
                     .contentType(APPLICATION_JSON)
                     .content(jsonBody))
-            .andExpect(status().isOk());
+                    .andExpect(status().isOk());
 
         // check result: nums of player & each name, id
         CitadelsGame actualGame = findById(game.getId());
+
         assertEquals(actualGame.getPlayers().size(), 3);
         assertTrue(actualGame.getPlayer("A").equals(A));
         assertTrue(actualGame.getPlayer("B").equals(B));
         assertTrue(actualGame.getPlayer("C").equals(C));
     }
 
-
-
     @Test
     public void playerJoinsGameUnsuccessfully() throws Exception {
+
         Player A = new Player("123", "A");
         Player B = new Player("456", "B");
         Player C = new Player("789", "C");
@@ -103,18 +52,17 @@ public class JoinGameTest {
         CitadelsGame game = givenGameStarted(A, B, C, D, E, F, G);
 
         // join game
-        // post /games/{gameId}:join
-        // body playerId, playerName
         Player H = new Player("789", "H");
         String jsonBody = "{\"playerName\" : \"" + H.getName() + "\", " +
-                "\"playerId\" : \"" + H.getId() + "\" }";
-        System.out.println(game.getId() + "-UnitTest");
+                           "\"playerId\" : \"" + H.getId() + "\" }";
         mockMvc.perform(post("/games/{gameId}:join", game.getId())
                         .contentType(APPLICATION_JSON)
                         .content(jsonBody))
-                .andExpect(status().isServiceUnavailable());
+                        .andExpect(status().isBadRequest())
+                        .andExpect(content().string("GAME IS FULL"));
 
         CitadelsGame actualGame = findById(game.getId());
+
         assertEquals(actualGame.getPlayers().size(), 7);
         assertTrue(actualGame.getPlayer("A").equals(A));
         assertTrue(actualGame.getPlayer("B").equals(B));
@@ -123,6 +71,5 @@ public class JoinGameTest {
         assertTrue(actualGame.getPlayer("E").equals(E));
         assertTrue(actualGame.getPlayer("F").equals(F));
         assertTrue(actualGame.getPlayer("G").equals(G));
-
     }
 }
