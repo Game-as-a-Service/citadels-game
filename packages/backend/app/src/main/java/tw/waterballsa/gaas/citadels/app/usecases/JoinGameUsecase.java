e432fdbc54c7414bb5a56f15baaccf741a74ebfa
chiashengchen <peterchen40213@yahoo.com.tw>
2023-08-18 22:05:12 +0800

    Add Usecase, Controller and Repo

---
+++ packages/backend/app/src/main/java/tw/waterballsa/gaas/citadels/app/usecases/JoinGameUsecase.java
@@ -0,0 +1,35 @@
+package tw.waterballsa.gaas.citadels.app.usecases;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import tw.waterballsa.gaas.citadels.app.repositories.CitadelsGameRepository;
+import tw.waterballsa.gaas.citadels.domain.CitadelsGame;
+import tw.waterballsa.gaas.citadels.domain.Player;
+
+import javax.inject.Named;
+
+
+@Named
+@RequiredArgsConstructor
+public class JoinGameUsecase {
+
+    private final CitadelsGameRepository citadelsGameRepository;
+
+    public void execute(Request request) {
+        CitadelsGame game = citadelsGameRepository.findById(request.getGameId());
+        Player player = new Player(request.getPlayerId(), request.getPlayerName());
+        citadelsGameRepository.save(game);
+    }
+
+    @Data
+    public static class Request {
+        private String playerId;
+        private String playerName;
+        private String gameId;
+        public Request(String gameId, String playerName, String playerId) {
+            this.gameId = gameId;
+            this.playerName = playerName;
+            this.playerId = playerId;
+        }
+    }
+}
--- packages/backend/spring/src/test/java/tw/waterballsa/gaas/citadels/spring/controllers/JoinGameTest.java
+++ packages/backend/spring/src/test/java/tw/waterballsa/gaas/citadels/spring/controllers/JoinGameTest.java
@@ -1,6 +1,6 @@
 package tw.waterballsa.gaas.citadels.spring.controllers;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,18 +8,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import tw.waterballsa.gaas.citadels.app.repositories.CitadelsGameRepository;
 import tw.waterballsa.gaas.citadels.domain.CitadelsGame;
 import tw.waterballsa.gaas.citadels.domain.Player;
-import tw.waterballsa.gaas.citadels.spring.repositories.dao.CitadelsGameDAO;
 
-import static org.assertj.core.util.Arrays.asList;
+import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.*;
-import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@SpringBootTest
-@AutoConfigureMockMvc
-public class JoinGameTest {
 
-    //    @Test Saboteur
+//    @Test Saboteur
 //    public void 都好的硬要修() throws Exception {
 //        Player A = defaultPlayer("A");
 //        Player B = defaultPlayer("B");
@@ -46,38 +44,65 @@ public class JoinGameTest {
 //        assertTrue(actualA.getTool(ToolName.LANTERN).isAvailable());
 //        assertTrue(actualA.getTool(ToolName.PICK).isAvailable());
 //    }
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class JoinGameTest {
+
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private CitadelsGameRepository gameRepository;
+    private CitadelsGameRepository citadelsGameRepository;
 
     private CitadelsGame givenGameStarted(Player ...players) {
-        return gameRepository.save(new CitadelsGame(asList(players)));
+        return citadelsGameRepository.save(new CitadelsGame(asList(players)));
     }
 
+    private CitadelsGame findById(String gameId) {
+        return citadelsGameRepository.findById(gameId);
+    }
+
+    // 房間的狀態要不要考慮 ?
     @Test
-    void playerJoinsGameSuccessfully() {
+    public void playerJoinsGameSuccessfully() throws Exception {
+
         // init game
-        Player A = new Player("A");
-        Player B = new Player("B");
+        Player A = new Player("123", "A");
+        Player B = new Player("456", "B");
         CitadelsGame game = givenGameStarted(A, B);
 
         // join game
-        Player C = new Player("C");
-        game.joinPlayer(C);
+        // post /games/{gameId}:join
+        // body playerId, playerName
+        Player C = new Player("789", "C");
+        String jsonBody = "{\"playerId\" : \"" + C.getName() + "\" " +
+                "\"playerId\" : \"" + C.getId() + "\" }";
+        mockMvc.perform(post("/games/{gameId}:join", game.getId())
+                    .contentType(APPLICATION_JSON)
+                    .content(jsonBody))
+            .andExpect(status().isOk());
 
-        // check result
-        assertEquals(game.getPlayers().size(), 3);
-        assertEquals(game.getPlayer("C").getName(), "C");
+        // check result: nums of player & each name, id
+        CitadelsGame actualGame = findById(game.getId());
+        assertEquals(actualGame.getPlayers().size(), 3);
+        assertEquals(actualGame.getPlayer("A").getName(), A.getName());
+        assertEquals(actualGame.getPlayer("A").getId(), A.getId());
+
+        assertEquals(actualGame.getPlayer("B").getName(), B.getName());
+        assertEquals(actualGame.getPlayer("B").getId(), B.getId());
+
+        assertEquals(actualGame.getPlayer("C").getName(), C.getName());
+        assertEquals(actualGame.getPlayer("C").getId(), C.getId());
     }
 
 
+
     @Test
-    void playerJoinsGameUnsuccessfully() {
-        Player A = new Player("A");
-        Player B = new Player("B");
+    public void playerJoinsGameUnsuccessfully() {
+        Player A = new Player("123", "A");
+        Player B = new Player("456", "B");
         CitadelsGame game = givenGameStarted(A, B);
-        Player C = new Player("C");
+        Player C = new Player("789", "C");
     }
 }
