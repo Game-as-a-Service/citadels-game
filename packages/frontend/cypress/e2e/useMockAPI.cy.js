fd75549a0bbc3dd9e1d6b2213f36d6c5d87ca1d8
Irisloverain <56682095+Irisloverain@users.noreply.github.com>
2023-09-02 01:12:42 +0800

    刪除不太必要的測試 (#101)
    Co-authored-by: Enya <26k1234o3@gmail.com>

--- packages/frontend/cypress/e2e/useMockAPI.cy.js
+++ packages/frontend/cypress/e2e/useMockAPI.cy.js
@@ -1,9 +1,6 @@
-// eslint-disable-next-line no-undef
 describe('E2E Tests to MockAPI', () => {
-  // eslint-disable-next-line no-undef
   beforeEach(() => {
     // 跑E2E測試時，intercept時會攔截XHR、fetch、跨域請求，將get方法呼叫的API改為放在fixtures資料夾的hello.json*
-    // eslint-disable-next-line no-undef
     cy.intercept(
       'GET',
       'https://001f08b9-acb7-4c3a-a54f-a9254b7e8e55.mock.pstmn.io/get?msg=hello',
@@ -13,17 +10,13 @@ describe('E2E Tests to MockAPI', () => {
     ).as('getAPI')
   })
 
-  // eslint-disable-next-line no-undef
   it('should load data from Local Mock API', () => {
-    // eslint-disable-next-line no-undef
     cy.visit('http://localhost:3001/')
 
-    // eslint-disable-next-line no-undef
     cy.wait('@getAPI', { timeout: 10000 })
       .its('response.body')
       .then((data) => {
         // 在這裡設定預期從API獲取到的數據
-        // eslint-disable-next-line no-undef
         expect(data.args.msg).to.equal('hello')
       })
   })
