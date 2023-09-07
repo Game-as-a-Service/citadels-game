13701382c0d599dbb87ea70b45e4e60dec8bd735
Irisloverain <56682095+Irisloverain@users.noreply.github.com>
2023-09-07 21:37:18 +0800

    導入TypeScript (#110)
    Co-authored-by: Enya <26k1234o3@gmail.com>

--- packages/frontend/js/components/App.jsx
+++ packages/frontend/js/components/App.jsx
@@ -1,9 +1,11 @@
 import getAPI from '../api/getAPI'
+import Hello from './Hello'
 
 const App = () => {
   const { data } = getAPI()
   return (
     <>
+      <Hello></Hello>
       <div>title:{data.msg}</div>
     </>
   )
