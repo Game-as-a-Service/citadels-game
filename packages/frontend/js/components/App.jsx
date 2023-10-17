7716258acb163404a5f92064b0cc9592b83e05f3
Enya <26k1234o3@gmail.com>
2023-10-17 20:23:58 +0800

    SVG TS

--- packages/frontend/js/components/App.jsx
+++ packages/frontend/js/components/App.jsx
@@ -1,11 +1,9 @@
 import getAPI from '../api/getAPI'
-import Hello from './Hello'
 
 const App = () => {
   const { data } = getAPI()
   return (
     <>
-      <Hello></Hello>
       <div>title:{data.msg}</div>
     </>
   )
