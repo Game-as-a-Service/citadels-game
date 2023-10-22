390181c4cb33f7292b87778979f963a5ce98e978
Enya <26k1234o3@gmail.com>
2023-10-22 20:08:13 +0800

    user資訊context

--- packages/frontend/js/components/App.jsx
+++ packages/frontend/js/components/App.jsx
@@ -1,19 +1,20 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import RoomList from './RoomList'
 import Game from './Game'
-
 import getAPI from '../api/getAPI'
-
+import { UserProvider } from '../contexts/UserContext'
 const App = () => {
   const { data } = getAPI()
   return (
     <>
       {/* <div>title:{data.msg}</div> */}
-      <Routes>
-        <Route path='/' element={<Navigate to='/rooms' />} />
-        <Route path='/rooms' element={<RoomList />} />
-        <Route path='/game/:roomId' element={<Game />} />
-      </Routes>
+      <UserProvider>
+        <Routes>
+          <Route path='/' element={<Navigate to='/rooms' />} />
+          <Route path='/rooms' element={<RoomList />} />
+          <Route path='/game/:roomId' element={<Game />} />
+        </Routes>
+      </UserProvider>
     </>
   )
 }
