5d2781ecd66c57d766810e651976114baf34e337
dawnglede <50606676+dawnglede@users.noreply.github.com>
2023-12-03 22:39:54 +0800

    update webpack build script

--- packages/frontend/js/api/index.ts
+++ packages/frontend/js/api/index.ts
@@ -6,7 +6,7 @@ type Domain = {
 }
 const domain: Domain = {
   development: 'https://001f08b9-acb7-4c3a-a54f-a9254b7e8e55.mock.pstmn.io',
-  production: ''
+  production: 'https://104.155.239.183'
 }
 
 const env = process.env.NODE_ENV === 'production' ? 'production' : 'development'
