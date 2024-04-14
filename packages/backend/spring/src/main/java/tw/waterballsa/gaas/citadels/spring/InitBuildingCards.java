7aa6721d622ddea74f6727b83902af6693c51ea0
chiashengchen <peterchen40213@yahoo.com.tw>
2024-04-14 10:11:36 +0800

    Save json to mongodb

--- packages/backend/spring/src/main/java/tw/waterballsa/gaas/citadels/spring/InitBuildingCards.java
+++ packages/backend/spring/src/main/java/tw/waterballsa/gaas/citadels/spring/InitBuildingCards.java
@@ -1,9 +1,10 @@
 package tw.waterballsa.gaas.citadels.spring;
 
+import org.bson.BsonArray;
+import org.bson.BsonValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
@@ -12,21 +13,26 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @Component
-public class InitBuildingCard implements CommandLineRunner {
+public class InitBuildingCards implements CommandLineRunner {
 
     private final MongoTemplate mongoTemplate;
 
     @Autowired
-    public InitBuildingCard(MongoTemplate mongoTemplate) {
+    public InitBuildingCards(MongoTemplate mongoTemplate) {
         this.mongoTemplate = mongoTemplate;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Resource resource = new ClassPathResource("BuildingCardInfo.json");
+        if(mongoTemplate.collectionExists("buildingCards")) {
+            return;
+        }
+        ClassPathResource resource = new ClassPathResource("BuildingCardInfo.json");
         String jsonContent = new String(Files.readAllBytes(Paths.get(resource.getURI())));
-        Document document = Document.parse(jsonContent);
-        mongoTemplate.save(document, "BuildingCards");
+        BsonArray bsonArray = BsonArray.parse(jsonContent);
+        for(BsonValue bsonValue : bsonArray) {
+            Document doc = Document.parse(bsonValue.asDocument().toJson());
+            mongoTemplate.save(doc, "buildingCards");
+        }
     }
-
 }
